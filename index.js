d8746b4900683136c2f71244d789690c91382c21
unknown <handoohyuck@hanmail.net>
2024-05-09 11:09:06 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://handoohyuck:dur951122!@bolierplate.qydq1yp.mongodb.net/?retryWrites=true&w=majority&appName=bolierplate',{
-    
+const config = require('./config/key')
+
+const { User } = require('./models/User');
+
+const app = express();
+const port = 3000;
+
+// Body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(config.mongoURI,{
 }).then(() => console.log('MongoDB Connected..'))
     .catch(err => console.log(err))
 
-app.get('/', (req,res) => res.send('Hello World!'))
+app.get('/', (req,res) => res.send('Hello World!word'))
+
+
+app.post('/register', async (req, res) => {
+    try {
+        const user = new User(req.body);
+        const userInfo = await user.save();
+        return res.status(200).json({ success: true });
+    } catch (err) {
+        return res.status(500).json({ success: false, error: err.message });
+    }
+})
+
+
 
 app.listen(port, () => console.log(`Example app port ${port}!`))
 
--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const userSchema = mongoose.Schema({
     },
     password:{
         type: String,
-        maxlength50
+        maxlength: 50
     },
     role: {
         type: Number,
